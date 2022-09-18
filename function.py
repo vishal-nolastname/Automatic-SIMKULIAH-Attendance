04fc91042dd88f7acc4f9d9bccfe89631cdcfb60
Vishal <vishal.drey@gmail.com>
2022-09-18 23:04:26 +0700

    change function for heroku

--- function.py
+++ function.py
@@ -15,7 +15,7 @@ def driver_setup():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    #driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    #driver = webdriver.Chrome()
     
     return driver
